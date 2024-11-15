e3b1fbc6ac530a55d51bf2e739ec3f9cd27088d6
Reggie Joseph <64855625+rjsmart021@users.noreply.github.com>
2024-11-15 10:57:17 -0600

    Update api-data.js

--- api-data.js
+++ api-data.js
@@ -1,3 +1,7 @@
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+.then(response => response.json())
+.then(data => console.log(data))
+.catch(error => console.error('Error:', error));
 let ts ="168180292982683"
 let publicKey = "";
 let hashVal = "afcbeldde7d32a25088b712beb8b3fe0";
