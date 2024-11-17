d45b511c2a4849691d0dafda7cc32df1edc06450
Reggie Joseph <64855625+rjsmart021@users.noreply.github.com>
2024-11-17 16:31:05 -0600

    Update api-data.js

--- api-data.js
+++ api-data.js
@@ -1,3 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import Header from './components/header/Header';
+import CharacterContainer from './components/marvels/CharacterContainer';
+import axios from 'axios';
+import { notification } from 'antd'
+
+export const UserContext = React.createContext();
+const App = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchMarvelCharacters = async() => {
+      try{
+        let baseUrl = `${process.env.REACT_APP_BASE_URL}${process.env.REACT_APP_API_KEY}`;
+        let result = await axios(baseUrl);
+        setItems(result.data.data.results)
+      } catch(ex) {
+        setItems([]);
+        notification.error({
+          message: 'Failed to fetch marvel characters',
+          description: ex.message,
+          placement: 'top',
+        });
+      } finally {
+        setIsLoading(false)
+      }   
+    }
+
+    fetchCharacters();
+  }, []);
+
+  return (
+    <div className="container">
+      <UserContext.Provider value={items}>
+        <Header/>
+        <CharacterContainer items={items} isLoading={isLoading}/>
+      </UserContext.Provider>
+    </div>
+  );
+}
+
+export default App;
+
 //1. Exploring Asynchronous JavaScript
 //Task 1: Obtaining API Key and Configuration
 http(s)://gateway.marvel.com/
